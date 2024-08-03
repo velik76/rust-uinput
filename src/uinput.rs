5f881ab45c5e99315ae5fcc3869c9734bdf80149
Uwe Strempel <u.strempel@gmail.com>
2024-08-03 22:21:35 +0200

    Fixed ioctl uinput access

--- src/uinput.rs
+++ src/uinput.rs
@@ -1,13 +1,16 @@
 use crate::test_scenario::TestScenario;
 use input_linux_sys::*;
+use libc::{c_int, c_ulong};
 use libc::{c_void, fdopen, fwrite};
 use linux_raw_sys::ioctl::UI_DEV_CREATE;
+use linux_raw_sys::ioctl::UI_DEV_DESTROY;
 use linux_raw_sys::ioctl::UI_DEV_SETUP;
 use linux_raw_sys::ioctl::UI_SET_EVBIT;
 use linux_raw_sys::ioctl::UI_SET_KEYBIT;
 use std::ffi::CString;
+use std::os::fd::AsFd;
 use std::{fs::File, os::fd::AsRawFd};
-
+use std::{mem, slice};
 // #[macro_use]
 // ioctl_write_ptr!(ui_dev_setup, UI_DEV_SETUP, 0, uinput_setup);
 // ioctl_write_ptr(ui_dev_setup, UI_DEV_SETUP, EV_KEY);
@@ -17,20 +20,60 @@ use std::{fs::File, os::fd::AsRawFd};
 /// Testing of ioctl() calls so as they are used in example in
 /// https://kernel.org/doc/html/v4.19/input/uinput.html
 ///
-pub fn ioctl_test() {
-    let file = File::open("/dev/uinput").unwrap();
-    let fd = file.as_raw_fd();
+ioctl_write_ptr_bad!(set_uinput_setup, UI_DEV_SETUP, uinput_setup);
+ioctl_none_bad!(ui_dev_create, UI_DEV_CREATE);
+ioctl_none_bad!(ui_dev_destroy, UI_DEV_DESTROY);
+ioctl_write_int!(ui_ioctl_set_evbit, UI_SET_EVBIT, EV_KEY);
+ioctl_write_int!(ui_ioctl_set_keybit, UI_SET_KEYBIT, KEY_K);
+const REPORT: input_event = input_event {
+    time: timeval {
+        tv_sec: 0,
+        tv_usec: 0,
+    },
+    type_: EV_SYN as u16,
+    code: SYN_REPORT as u16,
+    value: 0,
+};
+const KEY_K_PRESSED: input_event = input_event {
+    time: timeval {
+        tv_sec: 0,
+        tv_usec: 0,
+    },
+    type_: EV_KEY as u16,
+    code: KEY_K as u16,
+    value: 1,
+};
+const KEY_K_RELEASED: input_event = input_event {
+    time: timeval {
+        tv_sec: 0,
+        tv_usec: 0,
+    }, //16
+    type_: EV_KEY as u16, //2
+    code: KEY_K as u16,   //2
+    value: 0,             //4
+};
 
+pub fn ioctl_test() {
     unsafe {
-        if libc::ioctl(fd, u64::from(UI_SET_EVBIT), EV_KEY) < 0 {
+        let file = File::options().write(true).open("/dev/uinput").unwrap();
+        // ui_ioctl_set_evbit(file.as_raw_fd(), EV_KEY  as nix::sys::ioctl::ioctl_param_type,).expect("set_evbits key");
+        if libc::ioctl(file.as_raw_fd(), UI_SET_EVBIT as c_ulong, EV_KEY) < 0 {
             panic!("UI_SET_EVBIT");
         }
-        if libc::ioctl(fd, u64::from(UI_SET_KEYBIT), KEY_K) < 0 {
+        // ui_ioctl_set_keybit(file.as_raw_fd(), KEY_K.try_into().unwrap()).expect("set_keybits k");
+        if libc::ioctl(file.as_raw_fd(), UI_SET_KEYBIT as c_ulong, KEY_K) < 0 {
             panic!("UI_SET_KEYBIT");
         }
 
         // Prepare entries in uinput_setup
-        let mut usetup: uinput_setup = uinput_setup {
+        // must not be null
+        //let a = join!(b"Example device\0", [0; 79 - b"Example device\0".len()]);
+        let mut v: [u8; 80] = [0; 80];
+        let a = [0; 79 - b"Example device\0".len()];
+        v[..b"Example device\0".len()].copy_from_slice(b"Example device\0");
+        v[b"Example device\0".len() + 1..].copy_from_slice(&a);
+        let name: [i8; 80] = unsafe { mem::transmute_copy(&v) };
+        let usetup: uinput_setup = uinput_setup {
             id: input_id {
                 bustype: BUS_USB,
                 vendor: 0x1234,
@@ -38,18 +81,31 @@ pub fn ioctl_test() {
                 version: 0,
             },
             ff_effects_max: 0,
-            name: [0; 80],
+            name,
         };
 
-        // To C const void*
-        let ptr: *const uinput_setup = &usetup;
-        let voidptr = ptr as *const c_void;
-        if libc::ioctl(fd, u64::from(UI_DEV_SETUP), voidptr) < 0 {
-            panic!("UI_DEV_SETUP");
-        }
-        if libc::ioctl(fd, u64::from(UI_DEV_CREATE)) < 0 {
-            panic!("UI_DEV_CREATE");
-        }
+        set_uinput_setup(file.as_raw_fd(), &usetup).expect("UI_DEV_SETUP");
+        ui_dev_create(file.as_raw_fd()).expect("UI_DEV_CREATE");
+        // let ret = libc::ioctl(file.as_raw_fd(), UI_DEV_CREATE as c_ulong);
+        // println!("ret={:?}", ret);
+        libc::sleep(1);
+        let key: [u8; 24] = unsafe { mem::transmute(KEY_K_PRESSED) };
+        nix::unistd::write(file.as_fd(), &key).expect("write failed");
+
+        let report: [u8; 24] = unsafe { mem::transmute(REPORT) };
+        nix::unistd::write(file.as_fd(), &report).expect("write failed");
+
+        let key: [u8; 24] = unsafe { mem::transmute(KEY_K_RELEASED) };
+        nix::unistd::write(file.as_fd(), &key).expect("write failed");
+
+        nix::unistd::write(file.as_fd(), &report).expect("write failed");
+
+        libc::sleep(1);
+        ui_dev_destroy(file.as_raw_fd()).expect("UI_DEV_DESTROY");
+        // if libc::ioctl(file.as_raw_fd(), UI_DEV_DESTROY as c_ulong) < 0 {
+        //     panic!("UI_DEV_CREATE");
+        // }
+        println!("\u{23CE}");
     }
 }
 
